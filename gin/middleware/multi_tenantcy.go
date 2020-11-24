303443b579f44cdf82f5b1e02b5fc3a222e0df61
Goxiaoy <xy1996mail@gmail.com>
2020-11-25 01:34:31 +0800

    implement gin middleware

---
+++ gin/middleware/multi_tenantcy.go
@@ -0,0 +1,48 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/goxiaoy/go-saas/common"
+	"github.com/goxiaoy/go-saas/common/http"
+)
+
+func MultiTenancy(key string,hmtOptF http.PatchHttpMultiTenancyOption,trOptF common.PatchTenantResolveOption,ts *common.TenantStore) gin.HandlerFunc {
+	if key == ""{
+		key="_tenant"
+	}
+	return func(context *gin.Context) {
+
+		hmtOpt := http.NewMultiTenancyOption("")
+		if hmtOptF != nil{
+			//patch
+			hmtOptF(hmtOpt)
+		}
+		trOpt := common.NewTenantResolveOption(
+			//TODO route
+			http.NewCookieTenantResolveContributor(*hmtOpt,context.Request),
+			http.NewFormTenantResolveContributor(*hmtOpt,context.Request),
+			http.NewHeaderTenantResolveContributor(*hmtOpt,context.Request),
+			http.NewQueryTenantResolveContributor(*hmtOpt,context.Request),
+			)
+		if trOptF != nil{
+			//patch
+			trOptF(trOpt)
+		}
+		//get tenant config
+		tenantConfigProvider := common.NewDefaultTenantConfigProvider(common.NewDefaultTenantResolver(*trOpt),*ts)
+		tenantConfig,err := tenantConfigProvider.Get(context)
+		if err!=nil{
+			//not found
+			context.AbortWithError(404,err)
+		}
+		//set current tenant
+		currentTenant :=common.ContextCurrentTenant{}
+		newContext,cancel := currentTenant.Change(context,tenantConfig.Id,tenantConfig.Name)
+		//cancel
+		defer cancel()
+		//with newContext
+		context.Request.WithContext(newContext)
+		//next
+		context.Next()
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,17 @@
-module github.com/Goxiaoy/go-saas
+module github.com/goxiaoy/go-saas
 
 go 1.15
+
+require (
+	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/ugorji/go v1.2.0 // indirect
+	golang.org/x/crypto v0.0.0-20201117144127-c1f2f97bffc9 // indirect
+	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+)
